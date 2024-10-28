989fa77630d31f1875751941cb6244bf11da42c1
Jeremy Mart Valentino Nainggolan <jeremy.nainggolann@gmail.com>
2024-10-28 14:59:17 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $data['page_title'] = 'JKN - Web';
+        return view('user.index', compact('data'));
+    }
+
+    public function bill() {
+        $data['page_title'] = 'Bill';
+        return view('user.bill', compact('data'));
+    }
+
+    public function my_voucher() {
+        $data['page_title'] = 'My Voucher';
+        return view('user.my-voucher', compact('data'));
+    }
+}
---
+++ resources/views/user/my-voucher.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('title', $data['page_title'])
+@section('content')
+    <x-nav-bar></x-nav-bar>
+    <div class="col-lg-12">
+        <div class="row">
+
+            <div class="card row m-4 border-0 rounded-3 shadow-lg fw-bolder fs-5">
+                <div class="bg-success pt-2 rounded-top-2">
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
