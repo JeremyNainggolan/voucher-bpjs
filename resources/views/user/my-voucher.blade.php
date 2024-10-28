9cba880704408d47438713844f59f93c6030f51b
Jeremy Mart Valentino Nainggolan <jeremy.nainggolann@gmail.com>
2024-10-28 23:16:57 +0700

    Update my-voucher.blade.php

--- resources/views/user/my-voucher.blade.php
+++ resources/views/user/my-voucher.blade.php
@@ -2,18 +2,17 @@
 @section('title', $data['page_title'])
 @section('content')
     <x-nav-bar></x-nav-bar>
-
-
-    <div class="container">
-        <div class="row gap-3 justify-content-center">
-            <div class="card col-lg-2 bg-light border-0 rounded-2 shadow">
-                <div class="fw-bolder">
-                    <h4 class="fs-4 mt-2 card-title">Diskon</h4>
-                    <p class="card-title">10% s.d. Rp100rb</p>
+    <div class="card border-0 px-1 m-5 rounded-4 shadow bg-body-tertiary col-sm-8 col-md-10 col-xl-2 ">
+        <div class="p-4">
+            <h2 class="fw-bold fs-2 mt-1">Claim Voucher</h2>
+            <form class="row g-3">
+                <div class="col-auto">
+                    <label for="voucher" class="mt-1">Tukarkan Kode Voucher Anda</label>
+                    <input type="text" class="form-control my-2" id="voucher" name="voucher" placeholder="Kode Voucher">
+                    <button type="submit" class="btn btn-primary mt-4">Confirm identity</button>
                 </div>
-                <p class="card-text">Berlaku hingga</p>
-                <p class="card-text mb-2">17 September 2024</p>
-            </div>
+            </form>
         </div>
     </div>
+    <hr>
 @endsection
